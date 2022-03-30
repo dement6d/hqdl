08f3a387199056b59c5e6241b28ee48ea2911c95
dement6d <cuff.ho@gmail.com>
2022-03-30 02:24:48 +0200

    rename to hqdl
    update csproj

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ rm -r builds
 mkdir builds
 
 cd src
-dotnet publish -c Release -r win-x64 --self-contained true
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
 mv bin/Release/net6.0/win-x64/publish ../builds/win-x64
 
-dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
 mv bin/Release/net6.0/linux-x64/publish ../builds/linux-x64
