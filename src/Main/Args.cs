34172935ad72eb73c0e697fbcdad99b12ff98d94
dement6d <cuff.ho@gmail.com>
2022-05-21 00:38:41 +0200

    Add description for values arg

--- src/Main/Args.cs
+++ src/Main/Args.cs
@@ -5,7 +5,7 @@ namespace Main
     public class Args
     {
         public static DirectoryArgument output = new DirectoryArgument('o', "output", "Path to output files to");
-        public static ValueArgument<string> values = new ValueArgument<string>('v', "values");
+        public static ValueArgument<string> values = new ValueArgument<string>('v', "values", "Values to download from, example: \"809943207680802848 & 207930882294546432\"");
 
     }
 }
